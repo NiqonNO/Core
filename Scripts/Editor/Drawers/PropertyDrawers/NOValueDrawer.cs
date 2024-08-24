673c3f7138b2259117cc638eb5fdd67ccfece7c7
Nikodem Ocios <niqon.no@gmail.com>
2024-08-24 21:55:49 +0200

    Multiline attribute for NOStringValues

---
+++ Scripts/Editor/Drawers/PropertyDrawers/NOValueDrawer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace NiqonNO.Core.Editor.PropertyDrawers
+{
+    public class NOValueDrawer<T1, T2, T3> : OdinValueDrawer<T1> where T1 : NOValue<T2, T3> where T3 : NOValueAsset<T2>
+    {
+        InspectorProperty UseReferenceProperty;
+        InspectorProperty LocalValueProperty;
+        InspectorProperty LocalReferenceProperty;
+        
+        internal static readonly ScrollableTextAreaInternalDelegate EditorGUI_ScrollableTextAreaInternal;
+        internal static readonly FieldInfo EditorGUI_s_TextAreaHash_Field;
+        internal static readonly int EditorGUI_s_TextAreaHash;
+        internal static readonly string[] popupOptions = { "Use Reference", "Use Variable" };
+        internal static readonly GUIStyle popupStyle;
+        
+        static NOValueDrawer()
+        {
+            MethodInfo method = typeof (EditorGUI).GetMethod("ScrollableTextAreaInternal", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            if (method != null)
+                EditorGUI_ScrollableTextAreaInternal = (ScrollableTextAreaInternalDelegate) Delegate.CreateDelegate(typeof (ScrollableTextAreaInternalDelegate), method);
+            EditorGUI_s_TextAreaHash_Field = typeof (EditorGUI).GetField("s_TextAreaHash", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            if (!(EditorGUI_s_TextAreaHash_Field != null))
+                return;
+            EditorGUI_s_TextAreaHash = (int) EditorGUI_s_TextAreaHash_Field.GetValue(null);
+            popupStyle = new GUIStyle(GUI.skin.GetStyle("StaticDropdown"))
+                { imagePosition = ImagePosition.ImageOnly};
+        }
+
+        protected override void Initialize()
+        {
+            UseReferenceProperty = Property.Children["UseReference"];
+            LocalValueProperty = Property.Children["LocalValue"];
+            LocalReferenceProperty = Property.Children["LocalReference"];
+        }
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            SirenixEditorGUI.BeginHorizontalPropertyLayout(label ?? GUIContent.none);
+            
+            bool popupResult = EditorGUILayout.Popup((bool)UseReferenceProperty.ValueEntry.WeakSmartValue ? 0 : 1, 
+                popupOptions, popupStyle, GUILayout.Width(10), GUILayout.MinHeight(15)) == 0;
+            UseReferenceProperty.ValueEntry.WeakSmartValue = popupResult;
+
+            if (popupResult) LocalReferenceProperty.Draw(GUIContent.none);
+            else LocalValueProperty.Draw(GUIContent.none);
+            
+            SirenixEditorGUI.EndHorizontalPropertyLayout();
+        }
+        
+        internal delegate string ScrollableTextAreaInternalDelegate(
+            Rect position,
+            string text,
+            ref Vector2 scrollPosition,
+            GUIStyle style);
+    }
+}
---
+++ Scripts/Utility/Attributes/NOStringValueMultilineAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NiqonNO.Core.Utility.Attributes
+{
+    public class NOStringValueMultilineAttribute : Attribute
+    {
+        public readonly int minLines;
+        public readonly int maxLines;
+        
+        public NOStringValueMultilineAttribute()
+        {
+            this.minLines = 3;
+            this.maxLines = 3;
+        }
+        
+        public NOStringValueMultilineAttribute(int minLines, int maxLines)
+        {
+            this.minLines = minLines;
+            this.maxLines = maxLines;
+        }
+    }
+}
