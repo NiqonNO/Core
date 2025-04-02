93fc457eca81c946de9959cecfe1c35dcbbff408
Nikodem Ocios <niqon.no@gmail.com>
2025-04-02 22:48:37 +0200

    Added typeless comparision of names in Unique Name Ensurer

--- Scripts/Utility/NONamingUtility.cs
+++ Scripts/Utility/NONamingUtility.cs
@@ -5,6 +5,16 @@ namespace NiqonNO.Core.Utility
 {
     public class NONamingUtility
     {
+        public static string EnsureUniqueName(Object[] collection, string compareName)
+        {
+            return EnsureUniqueNameRecursive();
+            string EnsureUniqueNameRecursive(int recursiveCount = 0)
+            {
+                string checkName = recursiveCount == 0 ? compareName : $"{compareName}_{recursiveCount}";
+                return collection.Any(child => child.name.Equals(checkName)) ?
+                    EnsureUniqueNameRecursive(++recursiveCount) : checkName;
+            }
+        }
         public static string EnsureUniqueName(Object[] collection, Object compareItem)
         {
             return EnsureUniqueNameRecursive();
