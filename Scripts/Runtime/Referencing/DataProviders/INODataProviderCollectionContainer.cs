b544243cc677e3680c47a871c97d26dfdd504333
Nikodem Ocios <niqon.no@gmail.com>
2025-06-29 15:23:43 +0200

    Typo fix

--- Scripts/Runtime/Referencing/DataProviders/INODataProviderCollectionContainer.cs
+++ Scripts/Runtime/Referencing/DataProviders/INODataProviderCollectionContainer.cs
@@ -2,6 +2,6 @@
 {
     public interface INODataProviderCollectionContainer
     {
-        public NODataProvider DataCollection { get; }
+        public NODataProviderCollectionBase DataCollection { get; }
     }
 }
