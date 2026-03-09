546262441a0211f24b6d29d621cd4e82c0377ce7
Nikodem Ocios <niqon.no@gmail.com>
2026-03-10 00:18:47 +0100

    Gather all function for DataSet

--- Scripts/Runtime/Core/ScriptableObjects/NODataSet.cs
+++ Scripts/Runtime/Core/ScriptableObjects/NODataSet.cs
@@ -8,5 +8,26 @@ namespace NiqonNO.Core
 	{
 		[field: SerializeField] 
 		public List<TData> Assets { get; private set; }
+		
+#if UNITY_EDITOR
+		[Sirenix.OdinInspector.Button]
+		void GatherAll()
+		{
+			string typeName = typeof(TData).Name;
+			string[] guids = UnityEditor.AssetDatabase.FindAssets($"t:{typeName}");
+			
+			Assets.Clear();
+
+			foreach (string guid in guids)
+			{
+				string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+				TData data = UnityEditor.AssetDatabase.LoadAssetAtPath<TData>(path);
+				if (data != null)
+					Assets.Add(data);
+			}
+
+			Debug.Log($"Found {guids.Length} assets of type {typeName}, assigned {Assets.Count} of them.");
+		}
+#endif
 	}
 }
