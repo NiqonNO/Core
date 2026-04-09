8bd1d5423e4c7a92b85aa00d2d8c3bb5f4655a22
Nikodem Ocios <niqon.no@gmail.com>
2026-04-09 18:30:42 +0200

    Fixed Sprite Preview drawer to work with packed sprites

--- Scripts/Editor/Drawers/Attribute Drawers/NOSpritePreviewAttributeDrawer.cs
+++ Scripts/Editor/Drawers/Attribute Drawers/NOSpritePreviewAttributeDrawer.cs
@@ -9,11 +9,21 @@ namespace NiqonNO.Core.Editor.Drawers.AttributeDrawers
         {
             CallNextDrawer(label);
             
-            if (ValueEntry.SmartValue == null) return;
+            var sprite = ValueEntry.SmartValue;
+            if (sprite == null) return;
 
+            var tex = sprite.texture;
+            var r = sprite.textureRect;
+            var uv = new Rect(
+                r.x / tex.width,
+                r.y / tex.height,
+                r.width / tex.width,
+                r.height / tex.height
+            );
+            
             GUILayout.BeginHorizontal(GUI.skin.box);
-            Rect rect = GUILayoutUtility.GetAspectRect((float)ValueEntry.SmartValue.texture.width / ValueEntry.SmartValue.texture.height);
-            GUI.DrawTexture(rect, ValueEntry.SmartValue.texture, ScaleMode.ScaleToFit);
+            Rect rect = GUILayoutUtility.GetAspectRect(r.width / r.height);
+            GUI.DrawTextureWithTexCoords(rect, tex, uv, true);
             GUILayout.EndHorizontal();
         }
     }
